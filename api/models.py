484eb938994e415c78617b589fffcc4ddb11f059
ma-chang <akihisa.bamba.10@gmail.com>
2022-04-09 11:29:25 +0900

    created Post and Comment Models

--- api/models.py
+++ api/models.py
@@ -1,8 +1,23 @@
+import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.conf import settings
 
+
+# avatar画像のアップロード先パス
+def upload_avatar_path(instance, filename):
+    extension = filename.split('.')[-1]
+    return '/'.join(['avatars', str(instance.userProfile.id)+str(instance.nickName)+str('.')+str(extension)])
+
+
+# postした画像のアップロード先パス
+def upload_post_path(instance, filename):
+    extension = filename.split('.')[-1]
+    return '/'.join(['posts', str(instance.userPost.id)+str(instance.title)+str('.')+str(extension)])
+
 # Create your models here.
+
+
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None):
         """
@@ -34,6 +49,7 @@ class UserManager(BaseUserManager):
 
         return user
 
+
 class User(AbstractBaseUser):
     email = models.EmailField(
         verbose_name='email address',
@@ -49,3 +65,46 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.email
+
+
+class Profile(models.Model):
+    nickName = models.CharField(max_length=20)
+    userProfile = models.OneToOneField(
+        settings.AUTH_USER_MODEL, related_name='userProfile', on_delete=models.CASCADE
+    )
+
+    create_on = models.DateTime(auto_now_add=True)
+    img = models.ImageField(blank=True, null=True,
+                            upload_to=upload_avatar_path)
+
+    def __str__(self):
+        return self.nickName
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    """
+    関連させるモデルのオブジェクトが一つしかないなら ForeignKey に、複数なら ManyToManyField にします。
+    only many_to_one is allowed
+    source url: https://docs.djangoproject.com/en/3.2/_modules/django/db/models/fields/related/#ForeignKey
+    """
+    userPost = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='userPost', on_delete=models.CASCADE
+    )
+    img = models.ImageField(blank=True, null=True, upload_to=upload_post_path)
+    liked = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='liked', blank=True)
+    create_on = models.DateTime(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=1000)
+    userComment = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='userComment', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
