1ea08d534429465c904f140583762c37bb6c09c1
ma-chang <akihisa.bamba.10@gmail.com>
2022-04-15 08:49:19 +0900

    implement env file and re-generated secret key

--- config/settings.py
+++ config/settings.py
@@ -11,18 +11,23 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+import environ
+
 from pathlib import Path
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Read env file
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1@i#za-#-va+1m$0#=!oi7*9as+odqg_!eduamuye0@ur=11v9"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
